3fd48d9e07f209f4f88399e52edae34276fbf097
CHAÂCHAÏ <CHACHA@CHAACHAI-PC>
2017-11-18 20:36:16 +0000

    Update du projet Stock

--- src/bean/Category.java
+++ src/bean/Category.java
@@ -8,6 +8,8 @@ package bean;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -20,6 +22,7 @@ public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     @OneToMany(mappedBy = "category")
