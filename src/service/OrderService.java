5dbb75de08e8512b9a843f961d70d4409e0a4ab5
CHAÂCHAÏ <CHACHA@CHAACHAI-PC>
2017-11-19 01:09:16 +0000

    Update du projet Stock

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -24,20 +24,20 @@ public class OrderService extends AbstractFacade<Commande> {
     }
 
     public void initDB() {
-        addOrder(5, "24/01/2015", "C01", "P05", 25000);
-        addOrder(2, "22/09/2016", "C06", "P01", 5000);
-        addOrder(1, "13/02/2017", "C03", "P06", 7500);
-        addOrder(1, "05/05/2017", "C02", "P02", 4500);
-        addOrder(1, "15/11/2017", "C05", "P03", 2300);
+        addOrder(5, "24/01/2015", "C01", "P05");
+        addOrder(2, "22/09/2016", "C06", "P01");
+        addOrder(1, "13/02/2017", "C03", "P06");
+        addOrder(1, "05/05/2017", "C02", "P02");
+        addOrder(1, "15/11/2017", "C05", "P03");
     }
 
-    public void addOrder(double quantity, String orderDate, String clientID, String productID, double totalPrice) {
+    public void addOrder(double quantity, String orderDate, String clientID, String productID) {
         Commande commande = new Commande();
         Client client = clientService.find(clientID);
         Product product = productService.find(productID);
         commande.setQuantity(quantity);
         commande.setOrderDate(orderDate);
-        commande.setTotalPrice(totalPrice);
+        commande.setTotalPrice(product.getUnitPrice()*quantity);
         commande.setClient(client);
         commande.setProduct(product);
         create(commande);
